ad47c598e737cc3760c81387085822bc4697e0eb
comfreestyle <vr_freestyle@outlook.com>
2015-08-28 02:13:17 +0700

    Create item_car layout

--- app/src/main/java/werock/com/material/adapters/CarAdapter.java
+++ app/src/main/java/werock/com/material/adapters/CarAdapter.java
@@ -1,7 +1,52 @@
 package werock.com.material.adapters;
 
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+import werock.com.material.domain.Car;
+
 /**
  * Created by Kotchaphan Muangsan on 28/8/2558.
  */
-public class CarAdapter {
+public class CarAdapter extends RecyclerView.Adapter<CarAdapter.MyViewHolder> {
+
+    private List<Car> carList;
+    private LayoutInflater layoutInflater;
+
+
+    public CarAdapter(Context context, List<Car> carList) {
+        this.carList = carList;
+        layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return super.getItemId(position);
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public MyViewHolder(View view) {
+            super(view);
+        }
+    }
 }
