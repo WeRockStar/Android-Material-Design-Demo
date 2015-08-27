f50fedb5daa1ab56ca0a93c15f7cc2b52ada89ea
comfreestyle <vr_freestyle@outlook.com>
2015-08-27 23:37:41 +0700

    Tool Display

--- app/src/main/java/werock/com/material/MainActivity.java
+++ app/src/main/java/werock/com/material/MainActivity.java
@@ -2,15 +2,23 @@ package werock.com.material;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar_main);
+        toolbar.setTitle("Main Activity");
+        //toolbar.setSubtitle("Subtitle");
+        setSupportActionBar(toolbar);
     }
 
     @Override
