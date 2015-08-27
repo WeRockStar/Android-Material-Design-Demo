118e7f227b84ad97587ee744749f2fa84083a6d8
comfreestyle <vr_freestyle@outlook.com>
2015-08-28 01:42:31 +0700

    Create fragment car

--- app/src/main/java/werock/com/material/fragments/CarFragment.java
+++ app/src/main/java/werock/com/material/fragments/CarFragment.java
@@ -1,7 +1,24 @@
 package werock.com.material.fragments;
 
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import werock.com.material.R;
+
 /**
  * Created by Kotchaphan Muangsan on 28/8/2558.
  */
-public class CarFragment {
+public class CarFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_car, container, false);
+
+        return view;
+    }
 }
