74adf1591476440c1905bb967f2a270c261e86e6
comfreestyle <vr_freestyle@outlook.com>
2015-08-28 02:44:09 +0700

    Change RecyclerViewOnClickListenerHack Class to Interface

--- app/src/main/java/werock/com/material/interfaces/RecyclerViewOnClickListenerHack.java
+++ app/src/main/java/werock/com/material/interfaces/RecyclerViewOnClickListenerHack.java
@@ -3,5 +3,5 @@ package werock.com.material.interfaces;
 /**
  * Created by Kotchaphan Muangsan on 28/8/2558.
  */
-public class RecyclerViewOnClickListenerHack {
+public interface RecyclerViewOnClickListenerHack {
 }
