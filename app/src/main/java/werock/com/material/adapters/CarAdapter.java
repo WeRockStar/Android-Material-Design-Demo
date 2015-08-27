7a913bf098b6d66e9a1db73e1e148e85ef314cb6
comfreestyle <vr_freestyle@outlook.com>
2015-08-28 02:04:40 +0700

    Create CarAdapter class

---
+++ app/src/main/java/werock/com/material/adapters/CarAdapter.java
@@ -0,0 +1,7 @@
+package werock.com.material.adapters;
+
+/**
+ * Created by Kotchaphan Muangsan on 28/8/2558.
+ */
+public class CarAdapter {
+}
--- app/src/main/java/werock/com/material/fragments/CarFragment.java
+++ app/src/main/java/werock/com/material/fragments/CarFragment.java
@@ -4,22 +4,47 @@ package werock.com.material.fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.List;
+
 import werock.com.material.R;
+import werock.com.material.domain.Car;
 
 /**
  * Created by Kotchaphan Muangsan on 28/8/2558.
  */
 public class CarFragment extends Fragment {
 
-    @Nullable
+    private RecyclerView recyclerView;
+    private List<Car> list;
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_car, container, false);
 
+        recyclerView = (RecyclerView) view.findViewById(R.id.rv_list);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+            }
+
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+            }
+        });
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(linearLayoutManager);
         return view;
     }
 }
